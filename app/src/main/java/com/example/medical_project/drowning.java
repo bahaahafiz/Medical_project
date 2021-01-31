c84bd281bc0d0ffaf976f296bd06eee28516f543
bahaahafiz <bahaahafiz1@gmail.com>
2021-01-31 14:27:20 +0200

    bahaa edit project2

---
+++ app/src/main/java/com/example/medical_project/drowning.java
@@ -0,0 +1,14 @@
+package com.example.medical_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class drowning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drowning);
+    }
+}
