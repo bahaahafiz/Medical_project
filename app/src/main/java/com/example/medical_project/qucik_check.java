eab12f63ab44c39f26006c7e1105d47cf7256011
bahaahafiz <bahaahafiz1@gmail.com>
2021-02-01 08:32:53 +0200

    final quick_check task

---
+++ app/src/main/java/com/example/medical_project/qucik_check.java
@@ -0,0 +1,14 @@
+package com.example.medical_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class qucik_check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qucik_check);
+    }
+}
